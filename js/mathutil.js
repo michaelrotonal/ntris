b9a45b2309494d5bf99f2a6357fd85e555bc71aa
Michael Darrow <michaelnonagon@gmail.com>
2026-07-29 21:29:52 -0500

    fixed the worst bug of all

--- js/mathutil.js
+++ js/mathutil.js
@@ -92,62 +92,65 @@ function addZeros(matrix) {
 }
 
 function toCentered(grid) {
-  let matrix = grid;
-  let Isum = 0;
-  let Jsum = 0;
-  let total = 0;
-  for (let i=0; i<matrix.length; i++) {
-    for (let j=0; j<matrix[i].length; j++) {
-      if (matrix[i][j]) {
-        Isum += i;
-        Jsum += j;
-        total += 1;
+  if (countCells(grid)) > 0 {
+    let matrix = grid.map(h => h.toSpliced());
+    let Isum = 0;
+    let Jsum = 0;
+    let total = 0;
+    for (let i=0; i<matrix.length; i++) {
+      for (let j=0; j<matrix[i].length; j++) {
+        if (matrix[i][j]) {
+          Isum += i;
+          Jsum += j;
+          total += 1;
+        }
       }
     }
-  }
-  let centercoords = [Isum / total, Jsum / total];
-  let dcc = [1 + (Math.round(centercoords[0] + centercoords[1]) + Math.round(centercoords[0] - centercoords[1])), 1 + (Math.round(centercoords[0] + centercoords[1]) - Math.round(centercoords[0] - centercoords[1]))];
-  if (dcc[0] > matrix.length) {
-    for (let i = matrix.length; i < dcc[0]; i++) {
-      matrix.push(new Array(matrix[0].length).fill(0));
-    }
-  } else {
-    for (let i = matrix.length; i > dcc[0]; i--) {
-      matrix.unshift(new Array(matrix[0].length).fill(0));
-    }
-  }
-  if (dcc[1] > matrix[0].length) {
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = matrix[i].length; j < dcc[1]; j++) {
-        matrix[i].push(0);
+    let centercoords = [Isum / total, Jsum / total];
+    let dcc = [1 + (Math.round(centercoords[0] + centercoords[1]) + Math.round(centercoords[0] - centercoords[1])), 1 + (Math.round(centercoords[0] + centercoords[1]) - Math.round(centercoords[0] - centercoords[1]))];
+    if (dcc[0] > matrix.length) {
+      for (let i = matrix.length; i < dcc[0]; i++) {
+        matrix.push(new Array(matrix[0].length).fill(0));
+      }
+    } else {
+      for (let i = matrix.length; i > dcc[0]; i--) {
+        matrix.unshift(new Array(matrix[0].length).fill(0));
       }
     }
-  } else {
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = matrix[i].length; j > dcc[1]; j--) {
-        matrix[i].unshift(0);
+    if (dcc[1] > matrix[0].length) {
+      for (let i = 0; i < matrix.length; i++) {
+        for (let j = matrix[i].length; j < dcc[1]; j++) {
+          matrix[i].push(0);
+        }
+      }
+    } else {
+      for (let i = 0; i < matrix.length; i++) {
+        for (let j = matrix[i].length; j > dcc[1]; j--) {
+          matrix[i].unshift(0);
+        }
       }
     }
-  }
-  if (matrix.length < matrix[0].length) {
-    while (matrix.length < matrix[0].length) {
-      matrix.push(new Array(matrix[0].length).fill(0));
-      matrix.unshift(new Array(matrix[0].length).fill(0));
-    }
-  } else {
-    while (matrix[0].length < matrix.length) {
-      for (let i=0; i < matrix.length; i++) {
-        matrix[i].push(0);
-        matrix[i].unshift(0);
+    if (matrix.length < matrix[0].length) {
+      while (matrix.length < matrix[0].length) {
+        matrix.push(new Array(matrix[0].length).fill(0));
+        matrix.unshift(new Array(matrix[0].length).fill(0));
+      }
+    } else {
+      while (matrix[0].length < matrix.length) {
+        for (let i=0; i < matrix.length; i++) {
+          matrix[i].push(0);
+          matrix[i].unshift(0);
+        }
       }
     }
-  }
-
-  while (matrix[0].indexOf(1) == -1 && matrix[matrix.length-1].indexOf(1) == -1 && matrix.every(trerr => trerr[0] == 0 && trerr[trerr.length-1] == 0)) {
-    matrix = matrix.slice(1, -1);
-    matrix = matrix.map(nswttt => nswttt.slice(1, -1));
-  }
+    while (matrix[0].indexOf(1) == -1 && matrix[matrix.length-1].indexOf(1) == -1 && matrix.every(trerr => trerr[0] == 0 && trerr[trerr.length-1] == 0)) {
+      matrix = matrix.slice(1, -1);
+      matrix = matrix.map(nswttt => nswttt.slice(1, -1));
+    }
   return matrix;
+  } else {
+    return [[1]]; // doesn't crash on empty array anymore!
+  }
 }
 
 function isGreater(array1, array2) {
